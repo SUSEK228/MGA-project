ed3a7cff264c56799ed2c2727cb0e7ee18634bc1
SUSEK228 <148262852+SUSEK228@users.noreply.github.com>
2025-05-21 23:11:08 +0200

    dodano filtrowanie/praca w curl

--- task_manager/task_manager/views.py
+++ task_manager/task_manager/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
 
 from taskapp.serializers import TaskSerializer
 from taskapp.models import Task
@@ -10,3 +11,6 @@ class TaskViewSet(viewsets.ModelViewSet):
     queryset=Task.objects.all()
     serializer_class=TaskSerializer
     
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['id', 'name', 'status', 'assigned_user']
+    search_fields = ['name', 'description']
